aa15a2e7b7dc5454d0af13982e117152f1050f69
Doug Reeves <guodman@gmail.com>
2012-02-11 20:17:16 -0600

    implemented the android app

--- dude-android/src/com/mortalpowers/dude/android/DudeAndroidActivity.java
+++ dude-android/src/com/mortalpowers/dude/android/DudeAndroidActivity.java
@@ -1,13 +1,31 @@
 package com.mortalpowers.dude.android;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class DudeAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.mortalpowers.dude.game.DudeGame;
+
+public class DudeAndroidActivity extends AndroidApplication {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		initialize(new DudeGame(), false);
+		
+	}
+
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+
+		// This line of code is a jerk.
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	}
 }
